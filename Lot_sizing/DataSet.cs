622f938c75a9c8562067575bcc3d8d282fd23848
Xelat Akcam <xelat258@gmail.com>
2017-06-06 19:42:19 +0200

    Network

---
+++ Lot_sizing/DataSet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lot_sizing.Network
+{
+	public class DataSet
+	{
+		#region -- Properties --
+		public double[] Values { get; set; }
+		public double[] Targets { get; set; }
+		#endregion
+
+		#region -- Constructor --
+		public DataSet(double[] values, double[] targets)
+		{
+			Values = values;
+			Targets = targets;
+		}
+		#endregion
+	}
+}
--- Lot_sizing/InputNeuronen.cs
+++ Lot_sizing/InputNeuronen.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Lot_sizing
+namespace Lot_sizing.Network
 {
 	class InputNeuronen
 	{
@@ -13,22 +13,28 @@ namespace Lot_sizing
 		public static int setupTime;
 		public static int period;
 		public static int demand;
-		public static List<int> inputNeuronen;
+		public static List<int> InputLayer;
 
-		public static void Main()
+
+		public static void InputLotSize()
 		{
-			inputNeuronen = new List<int>();
+			InputLayer = new List<int>();
 			Console.WriteLine("Gib die Werte der folgenden Parameter ein:");
+			Console.WriteLine();
 
 			SetupCost();
-		
+			Console.WriteLine();
+
 			SetupTime();
+			Console.WriteLine();
 
 			Period();
+			Console.WriteLine();
 
 			Demand();
+			Console.WriteLine();
 
-			Console.WriteLine($"Es sind {inputNeuronen.Count()} Inputs gespeichert");
+			Console.WriteLine($"Es sind {InputLayer.Count()} Inputs gespeichert");
 
 			Console.ReadLine();
 
@@ -36,22 +42,23 @@ namespace Lot_sizing
 
 		public static void SetupCost()
 		{
-			Console.Write("Rüstkosten:");
+			Console.Write("Rüstkosten in Euro:");
 			setupCost = Convert.ToInt32(Console.ReadLine());
 			Console.WriteLine($"Die Rüstkosten betragen {setupCost} Euro");
 
-			inputNeuronen.Add(setupCost);
-			
-			//TODO: Eine Ja/Nein Abfrage, ob die Werte stimmen...
+			InputLayer.Add(setupCost);
+
+			//TODO: Eine Ja/Nein Abfrage, ob die Werte stimmen
+			//TODO: Exception einfügen, d.h. bei fehlerhafter Eingabe neu eingeben
 		}
 
 		public static void SetupTime()
 		{
-			Console.Write("Rüstzeit:");
+			Console.Write("Rüstzeit in Minuten:");
 			setupTime = Convert.ToInt32(Console.ReadLine());
-			Console.WriteLine($"Die Rüstzeit beträgt {setupTime} Zeiteinheiten");
+			Console.WriteLine($"Die Rüstzeit beträgt {setupTime} Minuten");
 
-			inputNeuronen.Add(setupTime);
+			InputLayer.Add(setupTime);
 		}
 
 		public static void Period()
@@ -64,20 +71,18 @@ namespace Lot_sizing
 
 		public static void Demand()
 		{
-			int num;
+			Console.WriteLine();
 			Console.WriteLine($"Nachfrage der {period} Perioden:");
+			Console.WriteLine();
 
-			num = 0;
-			while(num<period)
+			for (int num = 1; num < (period+1); num++)
 			{
-				demand = Convert.ToInt32(Console.ReadLine());
-				inputNeuronen.Add(demand);
-
-				num += 1;
+				Console.WriteLine();
+				Console.WriteLine($"Geben Sie die Nachfrage für die Periode {num + 1} an:");
+				Console.WriteLine($"Periode {num}={demand = Convert.ToInt32(Console.ReadLine())} Mengeneinheiten");
+				InputLayer.Add(demand);
 			}
-			
 
-			
 		}
 
 	}
